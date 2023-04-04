94fddbead63c1cf73a22093db301f6b26ed0ef07
yuta.tobita <yu.ta12171217@gmail.com>
2023-04-04 23:19:47 +0900

    ひとまず設定ビューの数値を返る部分は完成、レイアウトはまだ

--- app/src/main/java/com/example/pomodorotimer/Views/SettingView.kt
+++ app/src/main/java/com/example/pomodorotimer/Views/SettingView.kt
@@ -3,19 +3,15 @@ package com.example.pomodorotimer.Views
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.material.Slider
-import androidx.compose.material.TextField
-import androidx.compose.runtime.*
-import androidx.compose.ui.text.input.KeyboardType
 import com.example.pomodorotimer.ViewModels.SettingViewModel
+
 
 @Composable
 fun SettingView(viewModel: SettingViewModel) {
@@ -24,38 +20,33 @@ fun SettingView(viewModel: SettingViewModel) {
 
         NumberPickerView(
             label = "作業時間",
-            value = viewModel.workTime.value,
+            initialValue = viewModel.workTime.value,
             range = 1..120,
-            onValueChange = { viewModel.workTime.value = it },
-            steps = 180
+            onValueChange = { viewModel.workTime.value = it }
         )
         NumberPickerView(
             label = "休憩時間",
-            value = viewModel.shortBreakTime.value,
+            initialValue = viewModel.shortBreakTime.value,
             range = 1..60,
-            onValueChange = { viewModel.shortBreakTime.value = it },
-            steps = 180
+            onValueChange = { viewModel.shortBreakTime.value = it }
         )
         NumberPickerView(
             label = "休憩時間（長）",
-            value = viewModel.longBreakTime.value,
+            initialValue = viewModel.longBreakTime.value,
             range = 1..180,
-            onValueChange = { viewModel.longBreakTime.value = it },
-            steps = 180
+            onValueChange = { viewModel.longBreakTime.value = it }
         )
         NumberPickerView(
             label = "作業と休憩のセット数",
-            value = viewModel.numberOfWorkBreakSets.value,
+            initialValue = viewModel.numberOfWorkBreakSets.value,
             range = 1..20,
-            onValueChange = { viewModel.numberOfWorkBreakSets.value = it },
-            steps = 180
+            onValueChange = { viewModel.numberOfWorkBreakSets.value = it }
         )
         NumberPickerView(
             label = "全てのセット数",
-            value = viewModel.numberOfAllSets.value,
+            initialValue = viewModel.numberOfAllSets.value,
             range = 1..20,
-            onValueChange = { viewModel.numberOfAllSets.value = it },
-            steps = 180
+            onValueChange = { viewModel.numberOfAllSets.value = it }
         )
         SwitchView("タイマー終了時のバイブ", viewModel.timerVibration.value) { viewModel.timerVibration.value = it }
         SwitchView("タイマー終了時のアラート", viewModel.timerAlert.value) { viewModel.timerAlert.value = it }
@@ -63,45 +54,32 @@ fun SettingView(viewModel: SettingViewModel) {
 }
 
 @Composable
-fun NumberPickerView(
-    label: String,
-    value: Int,
-    range: IntRange = 0..100,
-    onValueChange: (Int) -> Unit,
-    steps: Int = 10
-) {
-    var textFieldValue by remember { mutableStateOf(value.toString()) }
+fun NumberPickerView(label: String, initialValue: Int, range: IntRange, onValueChange: (Int) -> Unit) {
+    var pickerValue by remember { mutableStateOf(initialValue) }
 
     Column {
-        Row() {
-            Text(text = label)
+        Text(label)
 
-            TextField(
-                value = textFieldValue,
-                onValueChange = { newValue ->
-                    textFieldValue = newValue
-                    newValue.toIntOrNull()?.let {
-                        if (it in range) {
-                            onValueChange(it)
-                        }
-                    }
-                },
-                keyboardType = KeyboardType.Number,
-                label = { Text("Number Picker") }
-            )
+        Row {
+            Button(
+                onClick = { if (pickerValue > range.first) pickerValue -= 1 },
+                enabled = pickerValue > range.first
+            ) {
+                Text("-")
+            }
 
-            Slider(
-                value = value.toFloat(),
-                onValueChange = { newValue ->
-                    onValueChange(newValue.toInt())
-                    textFieldValue = newValue.toInt().toString()
-                },
-                valueRange = range.first.toFloat()..range.last.toFloat(),
-                steps = steps
-            )
+            Text(pickerValue.toString())
+
+            Button(
+                onClick = { if (pickerValue < range.last) pickerValue += 1 },
+                enabled = pickerValue < range.last
+            ) {
+                Text("+")
+            }
         }
     }
 }
+
 
 @Composable
 fun SwitchView(label: String, isChecked: Boolean, onCheckedChange: (Boolean) -> Unit) {
